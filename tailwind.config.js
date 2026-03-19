0498b4b6a5bc08bb54d798b56096958b08d941d1
Chisato-O <chisato.ota12@gmail.com>
2026-03-19 23:47:20 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,18 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.css",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        asaclear: {
+          orange: '#F16813',
+          blue: '#3BAFDA',
+          bg: '#F6EBDD',
+        }
+      }
+    },
   },
   plugins: [],
 }
